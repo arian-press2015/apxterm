4d81293c5d5e49d10121b0635f07d307bf6d4d77
arian-press2015 <arian.press2015@gmail.com>
2025-02-21 20:25:23 +0330

    refactor(signals): move function up

--- src/signals.c
+++ src/signals.c
@@ -1,13 +1,13 @@
 #include "signals.h"
 #include "ui.h"
 
+void connect_signal(gpointer instance, const gchar *signal, GCallback callback, gpointer user_data)
+{
+    g_signal_connect(instance, signal, callback, user_data);
+}
+
 void activate_app(GtkApplication *app, gpointer user_data)
 {
     GtkWidget *window = create_main_window(app, "apXterm", 700, 400);
     gtk_widget_show_all(window);
-}
-
-void connect_signal(gpointer instance, const gchar *signal, GCallback callback, gpointer user_data)
-{
-    g_signal_connect(instance, signal, callback, user_data);
 }
