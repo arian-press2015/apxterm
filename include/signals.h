02fa0effe9e169f2cb13a08f16705074d7674371
arian-press2015 <arian.press2015@gmail.com>
2025-02-21 21:02:45 +0330

    feat(signals): add text_view and input to window

--- include/signals.h
+++ include/signals.h
@@ -3,8 +3,12 @@
 
 #include <gtk/gtk.h>
 
+void connect_signal(gpointer instance, const gchar *signal, GCallback callback, gpointer user_data);
+
 void activate_app(GtkApplication *app, gpointer user_data);
 
-void connect_signal(gpointer instance, const gchar *signal, GCallback callback, gpointer user_data);
+void send_input(GtkWidget *widget, gpointer data);
+
+void on_window_destroy(GtkWidget *widget, gpointer data);
 
 #endif
--- src/signals.c
+++ src/signals.c
@@ -9,5 +9,34 @@ void connect_signal(gpointer instance, const gchar *signal, GCallback callback, 
 void activate_app(GtkApplication *app, gpointer user_data)
 {
     GtkWidget *window = create_main_window(app, "apXterm", 700, 400);
+
+    GtkWidget *vbox = create_box(GTK_ORIENTATION_VERTICAL, 5);
+    append_to_container(GTK_CONTAINER(window), vbox);
+
+    GtkWidget *text_view = create_text_view(FALSE, GTK_WRAP_WORD_CHAR);
+    append_to_box(GTK_BOX(vbox), text_view, TRUE, TRUE, 0);
+
+    GtkWidget *entry = create_entry();
+    append_to_box(GTK_BOX(vbox), entry, FALSE, FALSE, 0);
+
+    connect_signal(entry, "activate", G_CALLBACK(send_input), text_view);
+
     gtk_widget_show_all(window);
+}
+
+void send_input(GtkWidget *widget, gpointer data)
+{
+    const char *input = gtk_entry_get_text(GTK_ENTRY(widget));
+    GtkWidget *text_view = GTK_WIDGET(data);
+
+    GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+
+    gtk_text_buffer_insert_at_cursor(buffer, input, -1);
+    gtk_text_buffer_insert_at_cursor(buffer, "\n", -1);
+
+    GtkTextIter end_iter;
+    gtk_text_buffer_get_end_iter(buffer, &end_iter);
+    gtk_text_view_scroll_to_iter(GTK_TEXT_VIEW(text_view), &end_iter, 0.0, TRUE, 0.0, 0.0);
+
+    gtk_entry_set_text(GTK_ENTRY(widget), "");
 }
