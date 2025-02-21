ff2a36c09e559d48e188030c6a65d6ad8c2aa3c4
arian-press2015 <arian.press2015@gmail.com>
2025-02-21 20:24:31 +0330

    refactor: break appending to box from creation of widget

--- include/ui.h
+++ include/ui.h
@@ -3,14 +3,16 @@
 
 #include <gtk/gtk.h>
 
-void append_widget(GtkContainer *container, GtkWidget *widget);
+void append_to_container(GtkContainer *container, GtkWidget *widget);
+
+void append_to_box(GtkBox *box, GtkWidget *widget, gboolean expand, gboolean fill, guint padding);
 
 GtkWidget *create_main_window(GtkApplication *app, char *name, int width, int height);
 
-GtkWidget *create_box(GtkApplication *app, GtkOrientation orientation, gint spacing);
+GtkWidget *create_box(GtkOrientation orientation, gint spacing);
 
-void append_text_view(GtkBox *box, gboolean expand, gboolean fill, guint padding);
+GtkWidget *create_text_view(GtkBox *box, gboolean expand, gboolean fill, guint padding);
 
-void append_entry(GtkBox *box, gboolean expand, gboolean fill, guint padding);
+GtkWidget *create_entry(GtkBox *box, gboolean expand, gboolean fill, guint padding);
 
 #endif
--- src/ui.c
+++ src/ui.c
@@ -1,9 +1,15 @@
 #include "ui.h"
 
-void append_widget(GtkContainer *container, GtkWidget *widget)
+void append_to_container(GtkContainer *container, GtkWidget *widget)
 {
     // use GTK_CONTAINER(some_widget) if container is a widget
     gtk_container_add(container, widget);
+}
+
+void append_to_box(GtkBox *box, GtkWidget *widget, gboolean expand, gboolean fill, guint padding)
+{
+    // use GTK_BOX(box_widget) if the box is of type GtkWidget
+    gtk_box_pack_start(box, widget, expand, fill, padding);
 }
 
 GtkWidget *create_main_window(GtkApplication *app, char *name, int width, int height)
@@ -15,24 +21,22 @@ GtkWidget *create_main_window(GtkApplication *app, char *name, int width, int he
     return window;
 }
 
-GtkWidget *create_box(GtkApplication *app, GtkOrientation orientation, gint spacing)
+GtkWidget *create_box(GtkOrientation orientation, gint spacing)
 {
-    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, spacing);
     return vbox;
 }
 
-void append_text_view(GtkBox *box, gboolean expand, gboolean fill, guint padding)
+GtkWidget *create_text_view(GtkBox *box, gboolean expand, gboolean fill, guint padding)
 {
     GtkWidget *text_view = gtk_text_view_new();
     gtk_text_view_set_editable(GTK_TEXT_VIEW(text_view), FALSE);
     gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(text_view), GTK_WRAP_WORD_CHAR);
-    // use GTK_BOX(box_widget) if the box is of type GtkWidget
-    gtk_box_pack_start(box, text_view, expand, fill, padding);
+    return text_view;
 }
 
-void append_entry(GtkBox *box, gboolean expand, gboolean fill, guint padding)
+GtkWidget *create_entry(GtkBox *box, gboolean expand, gboolean fill, guint padding)
 {
     GtkWidget *input_entry = gtk_entry_new();
-    // use GTK_BOX(box_widget) if the box is of type GtkWidget
-    gtk_box_pack_start(box, input_entry, expand, fill, padding);
+    return input_entry;
 }
